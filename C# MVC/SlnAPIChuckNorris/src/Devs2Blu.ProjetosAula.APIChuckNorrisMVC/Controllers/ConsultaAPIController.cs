5c6be0493b94604862c50943828b7e4b4e2d60b5
Pedro Henrique Godri <pedrogodri@gmail.com>
2022-11-29 21:46:45 -0300

    Commit - Consumindo API Chuck Norris em MVC

---
+++ C# MVC/SlnAPIChuckNorris/src/Devs2Blu.ProjetosAula.APIChuckNorrisMVC/Controllers/ConsultaAPIController.cs
@@ -0,0 +1,23 @@
+﻿using Devs2Blu.ProjetosAula.APIChuckNorrisMVC.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Devs2Blu.ProjetosAula.APIChuckNorrisMVC.Controllers
+{
+    [Route("consulta")]
+    public class ConsultaAPIController : Controller
+    {
+        private readonly ChuckNorrisAPIService Service = new ChuckNorrisAPIService();
+        public async Task<IActionResult> Index()
+        {
+            var result = await Service.GetPhraseChuck();
+            return View(result);
+        }
+
+        [Route("phrase")]
+        public PartialViewResult ChuckNorris()
+        {
+            var result = Service.GetPhraseChuck();
+            return PartialView();
+        }
+    }
+}
---
+++ C# MVC/SlnAPIChuckNorris/src/Devs2Blu.ProjetosAula.APIChuckNorrisMVC/Services/ChuckNorrisAPIService.cs
@@ -0,0 +1,53 @@
+﻿using Devs2Blu.ProjetosAula.APIChuckNorrisMVC.Models;
+
+namespace Devs2Blu.ProjetosAula.APIChuckNorrisMVC.Services
+{
+    public class ChuckNorrisAPIService
+    {
+        private readonly HttpClient _httpClient;
+        private const string URL_API_NORRIS_FOOD = "https://api.chucknorris.io/jokes/search?query=food";
+        
+        public ChuckNorrisAPIService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<ChuckNorris> GetPhraseChuck()
+        {
+            return await Get<ChuckNorris>(URL_API_NORRIS_FOOD);
+        }
+
+        public async Task<T> Get<T>(string url)
+        {
+            var listHttp = await GetAsync(url);
+
+            if (!listHttp.IsSuccessStatusCode)
+            {
+                return (T)(object)url;
+            }
+
+            return await listHttp.Content.ReadFromJsonAsync<T>();
+        }
+
+        public async Task<List<T>> GetList<T>(string url)
+        {
+            var listHttp = await GetAsync(url);
+
+            if (!listHttp.IsSuccessStatusCode)
+                return new List<T>();
+
+            return await listHttp.Content.ReadFromJsonAsync<List<T>>();
+        }
+
+        public async Task<HttpResponseMessage> GetAsync(string url)
+        {
+            var getRequest = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(url)
+            };
+            return await _httpClient.SendAsync(getRequest);
+        }
+
+    }
+}
