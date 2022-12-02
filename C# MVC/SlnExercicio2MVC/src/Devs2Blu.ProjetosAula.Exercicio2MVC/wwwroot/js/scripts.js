145ccd8536b479981188f295d4f25b05574811a8
Pedro Henrique Godri <pedrogodri@gmail.com>
2022-12-02 20:07:02 -0300

    Commit - Exercicio 2

---
+++ C# MVC/SlnExercicio2MVC/src/Devs2Blu.ProjetosAula.Exercicio2MVC/wwwroot/js/scripts.js
@@ -0,0 +1,12 @@
+﻿const getView = (controller, viewName, target, params = null) => {
+    let url = `/${controller}/${viewName}`;
+    url += (params) ? `${params}` : '';
+    let ajaxConfig = {
+        url: url,
+        dataType: 'html',
+        success: (response) => {
+            $(target).html(response);
+        }
+    };
+    $.ajax(ajaxConfig);
+}
