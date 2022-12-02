c3705425d5784ba3c8f5550b43f70525bb47e20d
Pedro Henrique Godri <pedrogodri@gmail.com>
2022-12-02 20:31:18 -0300

    Commit - Exluir exercicio

--- C# MVC/SlnExecicio2.1MVC/src/Devs2Blu.ProjetosAula.Exercicio2MVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Devs2Blu.ProjetosAula.Exercicio2MVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
