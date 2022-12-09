bcb0109695c1ff30f94c4b3468dbee027eac3415
Pedro Henrique Godri <pedrogodri@gmail.com>
2022-12-08 21:30:52 -0300

    Adicionando dois projetos MVC e SQL Server

---
+++ C# MVC/SlnMVCSqlServerApp2/src/Devs2Blu.ProjetosAula.MVCSQLServerApp2.Web/Models/ContextoDatabase.cs
@@ -0,0 +1,27 @@
+﻿using Devs2Blu.ProjetosAula.MVCSQLServerApp2.Web.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Devs2Blu.ProjetosAula.MVCSQLServerApp2.Web.Models
+{
+    public class ContextoDatabase : DbContext
+    {
+        public ContextoDatabase(DbContextOptions<ContextoDatabase> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Produto>()
+                .HasOne(p => p.Categoria)
+                .WithMany(c => c.Produtos);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        #region DbSets
+        public DbSet<Categoria> Categoria { get; set; }
+        public DbSet<Produto> Produto { get; set; }
+        #endregion
+    }
+}
---
+++ C# MVC/SlnMVCSqlServerApp2/src/Devs2Blu.ProjetosAula.MVCSQLServerApp2.Web/Models/Entities/Produto.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Devs2Blu.ProjetosAula.MVCSQLServerApp2.Web.Models.Entities
+{
+    [Table("produtos")]
+    public class Produto
+    {
+        [Column("id")]
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Column("nome")]
+        [Display(Name = "Nome")]
+        public string Nome { get; set; }
+
+        [Column("preco")]
+        [Display(Name = "Preco")]
+        public Decimal Preco { get; set; }
+
+        [Column("quantidade")]
+        [Display(Name = "Quantidade")]
+        public int Quantidade { get; set; }
+        
+        public Categoria Categoria { get; set; }
+    }
+}
