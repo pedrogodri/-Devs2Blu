38216a2e9e4a4adf539bbf6cd21f624131b2fec0
Pedro Henrique Godri <pedrogodri@gmail.com>
2022-12-14 20:14:24 -0300

    Adicionando - Repository e Services (Implements e Interfaces)

---
+++ C# MVC/SlnMVCSQLServerAppCompras/src/Devs2Blu.ProjetosAula.MVCSQLServerAppCompras.Web/Services/Implements/CategoriaService.cs
@@ -0,0 +1,21 @@
+﻿using Devs2Blu.ProjetosAula.MVCSQLServerAppCompras.Web.Models.Entities;
+using Devs2Blu.ProjetosAula.MVCSQLServerAppCompras.Web.Repository;
+using Devs2Blu.ProjetosAula.MVCSQLServerAppCompras.Web.Services.Interfaces;
+
+namespace Devs2Blu.ProjetosAula.MVCSQLServerAppCompras.Web.Services.Implements
+{
+    public class CategoriaService : ICategoriaService
+    {
+        private readonly CategoriaRepository _repository;
+
+        public CategoriaService(CategoriaRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<Categoria>> GetAllCategorias()
+        {
+            return await _repository.GetAll();
+        }
+    }
+}
---
+++ C# MVC/SlnMVCSQLServerAppCompras/src/Devs2Blu.ProjetosAula.MVCSQLServerAppCompras.Web/Services/Interfaces/ICategoriaService.cs
@@ -0,0 +1,9 @@
+﻿using Devs2Blu.ProjetosAula.MVCSQLServerAppCompras.Web.Models.Entities;
+
+namespace Devs2Blu.ProjetosAula.MVCSQLServerAppCompras.Web.Services.Interfaces
+{
+    public interface ICategoriaService
+    {
+            Task<IEnumerable<Categoria>> GetAllCategorias();
+    }
+}
