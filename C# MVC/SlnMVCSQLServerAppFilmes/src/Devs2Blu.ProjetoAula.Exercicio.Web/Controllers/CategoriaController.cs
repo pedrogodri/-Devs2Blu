330068a8397493f0c791f46ebc40b3d65c8b47de
Pedro Henrique Godri <pedrogodri@gmail.com>
2022-12-16 01:01:50 -0300

    Adicionando ao CategoriaController Repository, Interface e Implements

---
+++ C# MVC/SlnMVCSQLServerAppFilmes/src/Devs2Blu.ProjetoAula.Exercicio.Web/Controllers/CategoriaController.cs
@@ -0,0 +1,91 @@
+﻿using Devs2Blu.ProjetoAula.Exercicio.Web.Models.Entities;
+using Devs2Blu.ProjetoAula.Exercicio.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Devs2Blu.ProjetoAula.Exercicio.Web.Controllers
+{
+    public class CategoriaController : Controller
+    {
+
+        private readonly ICategoriaService _service;
+
+        public CategoriaController(ICategoriaService service)
+        {
+            _service = service;
+        }
+        // GET: CategoriaController
+        public async Task<ActionResult> Index()
+        {
+            var listCategorias = await _service.GetAllCategorias();
+            return View(listCategorias);
+        }
+
+        // GET: CategoriaController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CategoriaController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind("Id,Nome")] Categoria categoria)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    await _service.Save(categoria);
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch
+            {
+            }
+                return View(categoria);
+        }
+
+        // GET: CategoriaController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CategoriaController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CategoriaController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CategoriaController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
